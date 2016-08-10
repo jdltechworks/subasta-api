cb99d25d3721a47603e2b6e50634f31e50ab2069
jdltechworks <jdltechworks@gmail.com>
2016-08-10 08:06:45 +0800

    Adding bcrypt-nodejs

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,5 +1,10 @@
 /** @module  Controller */
 
+/**
+ * @todo  email verification
+ * @todo  actions for verified and error if user is not verified
+ */
+
 module.exports = {
 	/**
 	 * Overridden User create
