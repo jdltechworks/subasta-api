1aa8db8a1028a38709a9b9b794630e2a49817445
jdltechworks <jose@bywave.com.au>
2016-08-23 09:58:53 +0800

    upload test

--- api/controllers/AuctionController.js
+++ api/controllers/AuctionController.js
@@ -5,5 +5,20 @@
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
  */
 
+var path = require('path');
+
 module.exports = {
+	create: function(req, res, next) {
+		if(req.file('image')) {
+			req.file('image').upload({
+				dirname: process.cwd() + '/assets/upload'
+			}, (err, uploadedFiles) => {
+				if(err) res.serverError(err);
+
+				res.json({
+					files: uploadedFiles
+				});
+			});
+		}
+	}
 };
